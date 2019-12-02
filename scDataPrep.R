5213a3280ac24e9c980eed1faf22d7a6d48d102d
weihuaguo <gwh120104@gmail.com>
2019-12-02 10:53:25 -0800

    Create the script for preparation of scRNA-seq data

---
+++ scDataPrep.R
@@ -0,0 +1,9 @@
+# scDataPrep.R
+# Weihua Guo, Ph.D.
+# Start at 12/02/2019
+# Functions:
+# 1) Read raw count matrix with cell annotation
+# 2) Extract the cells of interest
+# 3) Imputation (optional)
+# 4) Batch correction
+# 5) Create a Seurat object for further analysis
