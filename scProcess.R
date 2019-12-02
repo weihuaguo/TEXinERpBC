6c03c63b0cdc684af5453a9b732d09b2ed043fd3
weihuaguo <gwh120104@gmail.com>
2019-12-02 15:01:44 -0800

    Create the script to run standard seurat pipeline

---
+++ scProcess.R
@@ -0,0 +1,9 @@
+# Single-cell RNA sequencing data standard processing procedure
+# Weihua Guo, Ph.D.
+# Start date: 12/02/2019
+# Functions: https://satijalab.org/seurat/v3.1/pbmc3k_tutorial.html
+
+rm(list = ls())
+
+suppressMessages(library(Seurat))
+
