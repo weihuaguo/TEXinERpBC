359ff5e36e04c631ac656318c057a2acda20263a
weihuaguo <gwh120104@gmail.com>
2019-12-02 16:08:48 -0800

    Start to write the standard pipeline

--- scProcess.R
+++ scProcess.R
@@ -7,3 +7,15 @@ rm(list = ls())
 
 suppressMessages(library(Seurat))
 
+dataDir = "/home/weihua/mnts/group_plee/Weihua/scrnaseq_leelab/scRNAseqTex/"
+ctsFile = "Tumor_LN_extracted_raw_counts.txt"
+cellAnnFile = "Tumor_LN_cell_annotation.txt"
+resDir = "/home/weihua/mnts/group_plee/Weihua/scrnaseq_results/scRNAseqTexResults/"
+
+expID = "tumor_ln_wo_scimp_test"
+dir.create(file.path(resDir, expID), showWarnings = TRUE)
+expDirPre = paste(resDir, expID, "/", expID, "_", sep = "")
+
+cat("Start to read counts...\n")
+ctsDf = read.table(paste(dataDir, ctsFile, sep = ""), header = TRUE, row.names = 1, sep = "\t")
+
