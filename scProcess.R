ef59ad511f08244fc66dcdf044d6ec373feef633
weihuaguo <gwh120104@gmail.com>
2020-01-21 16:42:48 -0800

    Finish the FACS data annotation

--- scProcess.R
+++ scProcess.R
@@ -1,7 +1,7 @@
 # Single-cell RNA sequencing data standard processing procedure
 # Weihua Guo, Ph.D.
 # Start date: 12/02/2019
-# NOTE: 01/21/2018 Add cell filter function here
+# NOTE: 01/21/2018 Add cell filter function, count-meta.data cell alignment, 
 # Functions: https://satijalab.org/seurat/v3.1/pbmc3k_tutorial.html
 
 rm(list = ls())
@@ -14,6 +14,7 @@ dataDir = "/home/weihua/mnts/group_plee/Weihua/scrnaseq_leelab/scRNAseqTex/Tumor
 # ctsFile = "Tumor_LN_extracted_raw_counts.txt"
 ctsFile = "scimpute_count.txt"
 cellAnnFile = "Tumor_LN_Normal_PBMC_Colorectal_Melanoma_cell_annotation.txt"
+facsTagFile = "FACS_TAG_ALL.txt"
 resDir = "/home/weihua/mnts/group_plee/Weihua/scrnaseq_results/scRNAseqTexResults"
 
 # expID = "tumor_crc_melanoma_scimp_k5_rmkeep_proenc_custMT_ndim_10"
@@ -40,6 +41,7 @@ cat("Start to read counts...\n")
 ctsDf = read.table(paste(dataDir, ctsFile, sep = ""), header = TRUE, row.names = 1, sep = " ")
 cellAnnDf = read.table(paste(dataDir, cellAnnFile, sep = ""), header = TRUE, row.names = 1, sep = " ",
 		       stringsAsFactors = FALSE)
+facsDf = read.table(paste(dataDir, facsTagFile, sep = ""), header = TRUE, row.names = 1, sep = "\t")
 
 ## TODO: move this out side finally!!!
 if (is.null(tisFilter)) {
@@ -55,18 +57,24 @@ if (is.null(tisFilter)) {
 	ctsDf = ctsDf[,rownames(cellAnnDf)]
 }
 
-print(head(cellAnnDf))
-print(ctsDf[1:9,1:6])
-print(dim(cellAnnDf))
-print(dim(ctsDf))
+## Add flow-cytometry tag
+facsDf = facsDf[rownames(facsDf) %in% colnames(ctsDf),]
+singleFACS = colnames(facsDf)
+# PD1-CD39 double positive
+facsDf$PD1CD39 = facsDf$PD1 + facsDf$CD39
+facsDf[facsDf$PD1CD39 == 1,"PD1CD39"] = 0
+facsDf[facsDf$PD1CD39 == 2,"PD1CD39"] = 1
 
-srsc = CreateSeuratObject(counts = ctsDf, project = expID, min.cells = 3, min.features = 200)
+metaDf = cbind(facsDf, cellAnnDf[,c("tissue", "plate")])
+metaDf[] = lapply(metaDf, factor)
+
+srsc = CreateSeuratObject(counts = ctsDf, project = expID, min.cells = 3, min.features = 200, meta.data = metaDf)
 # NOTE: before adding meta.data, align the rows of meta.data to ctsDf is NECCESSARY
-srsc@meta.data$tissue = as.factor(cellAnnDf[rownames(srsc@meta.data), "tissue"]) 
-srsc@meta.data$plate = as.factor(cellAnnDf[rownames(srsc@meta.data), "plate"])
+# srsc@meta.data$tissue = as.factor(cellAnnDf[rownames(srsc@meta.data), "tissue"]) 
+# srsc@meta.data$plate = as.factor(cellAnnDf[rownames(srsc@meta.data), "plate"])
 cat("Raw Seurat Object:")
 print(srsc)
-# print(head(srsc@meta.data))
+# stop("TT")
 
 if (custMTGeneFlag) {
 	cat("Use manually refined mitochondrial gene list to calculate percentage of mitochondrial gene expression\n")
@@ -89,7 +97,7 @@ ggsave(plot = qcComb, filename = paste(expDirPre, "QC_Correlation_2.tiff", sep =
        width = 12, height = 6, dpi = tifres)
 
 ctsData = srsc[["RNA"]]@data
-metaData = srsc@meta.data[,c("plate", "tissue")]
+metaData = srsc@meta.data[,c("plate", "tissue", "percent.mt")]
 
 if (rmFlag) {
 	cat("Remove the non-informative genes (ribosomal or mitochondrial genes...)\n")
@@ -119,7 +127,7 @@ if (keepFlag) {
 		srsc = subset(srsc, subset = percent.mt < 20)
 	} else {
 		ctsData = srsc[["RNA"]]@data
-		metaData = srsc@meta.data[,c("plate", "tissue")]
+		metaData = srsc@meta.data[,c("plate", "tissue", "percent.mt")]
 	}
 	##########################################################################
 	# Pre_filter
@@ -138,7 +146,11 @@ if (keepFlag) {
 }
 
 cat("After data clean:")
+print(head(colnames(srsc)))
+srsc[["FACS"]] = CreateAssayObject(counts = t(facsDf[colnames(srsc),]))
 print(srsc)
+print(head(srsc@meta.data))
+
 # NOTE: Normalization
 srsc = NormalizeData(srsc, normalization.method = "LogNormalize", scale.factor = 10000)
 # NOTE: Identify highly variable features
@@ -214,6 +226,19 @@ ggsave(plot = platePlot, filename = paste(expDirPre, "plate_umap_check_plot.tiff
 
 tisPlot = DimPlot(srsc, reduction = "umap", group.by = "tissue")
 ggsave(plot = tisPlot, filename = paste(expDirPre, "tissue_umap_check_plot.tiff", sep = ""), 
+       width = 7.5, height = 6, dpi = tifres)
+
+print(head(srsc@meta.data))
+facsPlot = FeaturePlot(srsc, reduction = "umap", features = singleFACS)
+ggsave(plot = facsPlot, filename = paste(expDirPre, "facs_single_marker_umap_check_plot.tiff", sep = ""), 
+       width = 15, height = 12, dpi = tifres)
+
+pd1cd39Plot = FeaturePlot(srsc, reduction = "umap", features = "PD1CD39")
+ggsave(plot = pd1cd39Plot, filename = paste(expDirPre, "exhausted_marker_umap_check_plot.tiff", sep = ""), 
+       width = 7.5, height = 6, dpi = tifres)
+
+mtPlot = FeaturePlot(srsc, reduction = "umap", features = "percent.mt")
+ggsave(plot = mtPlot, filename = paste(expDirPre, "mt_percent_umap_check_plot.tiff", sep = ""), 
        width = 7.5, height = 6, dpi = tifres)
 
 saveRDS(srsc, file = paste(expDirPre, "seurat_object.RDS", sep = ""))
